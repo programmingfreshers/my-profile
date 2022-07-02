1c4df12d1d9e676b6bb4b288b4735ff90150e3d7
programmingfreshers <77062180+programmingfreshers@users.noreply.github.com>
2022-07-02 17:01:19 +0530

    heroku

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <>
      <nav className="navbar bg-primary navbar-expand-lg ">
      <div className="container-fluid">
-     <a className="navbar-brand" href="/">Navbar</a>
+     <a className="navbar-brand" href="/">Samarth Goswami</a>
      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
      </button>
